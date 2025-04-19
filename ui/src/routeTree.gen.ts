53d34e35f069d268e41b551d166ea7991caeb3c3
mbeka02 <antonymbeka@gmail.com>
2025-04-19 10:02:32 +0300

    updated routes

--- ui/src/routeTree.gen.ts
+++ ui/src/routeTree.gen.ts
@@ -15,6 +15,7 @@ import { Route as AppRouteImport } from './routes/app/route'
 import { Route as IndexImport } from './routes/index'
 import { Route as AppPortfolioImport } from './routes/app/portfolio'
 import { Route as AppMarketplaceImport } from './routes/app/marketplace'
+import { Route as AppLearnImport } from './routes/app/learn'
 
 // Create/Update Routes
 
@@ -42,6 +43,12 @@ const AppMarketplaceRoute = AppMarketplaceImport.update({
   getParentRoute: () => AppRouteRoute,
 } as any)
 
+const AppLearnRoute = AppLearnImport.update({
+  id: '/learn',
+  path: '/learn',
+  getParentRoute: () => AppRouteRoute,
+} as any)
+
 // Populate the FileRoutesByPath interface
 
 declare module '@tanstack/react-router' {
@@ -59,6 +66,13 @@ declare module '@tanstack/react-router' {
       fullPath: '/app'
       preLoaderRoute: typeof AppRouteImport
       parentRoute: typeof rootRoute
+    }
+    '/app/learn': {
+      id: '/app/learn'
+      path: '/learn'
+      fullPath: '/app/learn'
+      preLoaderRoute: typeof AppLearnImport
+      parentRoute: typeof AppRouteImport
     }
     '/app/marketplace': {
       id: '/app/marketplace'
@@ -80,11 +94,13 @@ declare module '@tanstack/react-router' {
 // Create and export the route tree
 
 interface AppRouteRouteChildren {
+  AppLearnRoute: typeof AppLearnRoute
   AppMarketplaceRoute: typeof AppMarketplaceRoute
   AppPortfolioRoute: typeof AppPortfolioRoute
 }
 
 const AppRouteRouteChildren: AppRouteRouteChildren = {
+  AppLearnRoute: AppLearnRoute,
   AppMarketplaceRoute: AppMarketplaceRoute,
   AppPortfolioRoute: AppPortfolioRoute,
 }
@@ -96,6 +112,7 @@ const AppRouteRouteWithChildren = AppRouteRoute._addFileChildren(
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/app': typeof AppRouteRouteWithChildren
+  '/app/learn': typeof AppLearnRoute
   '/app/marketplace': typeof AppMarketplaceRoute
   '/app/portfolio': typeof AppPortfolioRoute
 }
@@ -103,6 +120,7 @@ export interface FileRoutesByFullPath {
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/app': typeof AppRouteRouteWithChildren
+  '/app/learn': typeof AppLearnRoute
   '/app/marketplace': typeof AppMarketplaceRoute
   '/app/portfolio': typeof AppPortfolioRoute
 }
@@ -111,16 +129,23 @@ export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
   '/app': typeof AppRouteRouteWithChildren
+  '/app/learn': typeof AppLearnRoute
   '/app/marketplace': typeof AppMarketplaceRoute
   '/app/portfolio': typeof AppPortfolioRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/app' | '/app/marketplace' | '/app/portfolio'
+  fullPaths: '/' | '/app' | '/app/learn' | '/app/marketplace' | '/app/portfolio'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/app' | '/app/marketplace' | '/app/portfolio'
-  id: '__root__' | '/' | '/app' | '/app/marketplace' | '/app/portfolio'
+  to: '/' | '/app' | '/app/learn' | '/app/marketplace' | '/app/portfolio'
+  id:
+    | '__root__'
+    | '/'
+    | '/app'
+    | '/app/learn'
+    | '/app/marketplace'
+    | '/app/portfolio'
   fileRoutesById: FileRoutesById
 }
 
@@ -154,9 +179,14 @@ export const routeTree = rootRoute
     "/app": {
       "filePath": "app/route.tsx",
       "children": [
+        "/app/learn",
         "/app/marketplace",
         "/app/portfolio"
       ]
+    },
+    "/app/learn": {
+      "filePath": "app/learn.tsx",
+      "parent": "/app"
     },
     "/app/marketplace": {
       "filePath": "app/marketplace.tsx",
