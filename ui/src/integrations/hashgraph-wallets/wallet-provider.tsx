02eb542086b6cd5eef50f7b2b1535b250ee2fabf
mbeka02 <antonymbeka@gmail.com>
2025-04-20 16:40:41 +0300

    feat: integrated hashgraph react wallets

---
+++ ui/src/integrations/hashgraph-wallets/wallet-provider.tsx
@@ -0,0 +1,34 @@
+import { HWBridgeProvider } from "@buidlerlabs/hashgraph-react-wallets";
+import { HWCConnector } from "@buidlerlabs/hashgraph-react-wallets/connectors";
+import {
+  HederaTestnet,
+  //   HederaMainnet,
+} from "@buidlerlabs/hashgraph-react-wallets/chains";
+import LoadingFallback from "@/components/ui/wallet-loading-fallback";
+
+// 1. Get projectId from https://cloud.reown.com
+const projectId =
+  import.meta.env.VITE_REOWN_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694";
+
+// 2. Create a metadata object - optional
+const metadata = {
+  name: "Tantei",
+  description:
+    "A place for developers to publish their agents and allow people to follow their trades for a commision",
+  url: window.location.href, // origin must match your domain & subdomain
+  icons: ["../../assets/logo/png/logo-no-background.png"],
+};
+
+export const WalletProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <HWBridgeProvider
+      metadata={metadata}
+      projectId={projectId}
+      connectors={[HWCConnector]}
+      chains={[HederaTestnet]}
+      LoadingFallback={LoadingFallback}
+    >
+      {children}
+    </HWBridgeProvider>
+  );
+};
