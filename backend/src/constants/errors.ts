65ae463012ba7a40dcb9bc5d9efa75844d9c909c
mbeka02 <antonymbeka@gmail.com>
2025-04-20 08:33:16 +0300

    updated custom errors

--- backend/src/constants/errors.ts
+++ backend/src/constants/errors.ts
@@ -1,12 +1,13 @@
 export class MyError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = "MyError";
-    }
+  constructor(message: string) {
+    super(message);
+    this.name = "MyError";
+  }
 }
 
 export enum Errors {
-    INVALID_SETUP = "Invalid server setup",
-    NOT_REGISTER_USER = "Could not register user",
-    NOT_GET_USER = "Could not get user"
+  INVALID_SETUP = "Invalid server setup",
+  NOT_REGISTER_USER = "Could not register user",
+  NOT_PUBLISH_AGENT = "Could not publish the agent",
+  NOT_GET_USER = "Could not get user",
 }
