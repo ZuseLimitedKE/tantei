c05720e70cc0f5848e10651b3c0a37c4c4c67322
mbeka02 <antonymbeka@gmail.com>
2025-04-21 20:33:30 +0300

    created the ui for the portfolio using mock data

--- ui/src/routes/app/route.tsx
+++ ui/src/routes/app/route.tsx
@@ -22,8 +22,8 @@ function AppLayoutComponent() {
       >
         <AppNavbar />
         <ChatDrawer>
-          <div className="rounded-full flex items-center justify-center p-1 w-14 h-14 bg-primary  text-white cursor-pointer  z-10 fixed right-10 bottom-10">
-            <IconMessage className="w-2/3 h-2/3" strokeWidth={1.75} />
+          <div className="rounded-full flex items-center justify-center p-1 w-14 h-14 bg-primary  text-white cursor-pointer  z-10 fixed right-5 bottom-5">
+            <IconMessage className="w-2/3 h-2/3" strokeWidth={1.5} />
           </div>
         </ChatDrawer>
       </Suspense>
