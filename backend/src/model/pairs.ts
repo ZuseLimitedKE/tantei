5f8f43d1fafba9c3c7fd0b2310b214fb77324edd
Roman Njoroge <roman.njoroge@njuguna.com>
2025-04-23 13:24:48 +0300

    feat: Function for updating pair price throws error if pair does not have 2 items

--- backend/src/model/pairs.ts
+++ backend/src/model/pairs.ts
@@ -20,15 +20,31 @@ export class PairsModel {
             throw new MyError(Errors.NOT_STORE_PAIR);
         }
     }
+
+    async updatePairPrice(price: number, pair: string[]) {
+        try {
+            if (pair.length !== 2) {
+                throw new MyError(Errors.INVALID_PAIR);
+            }
+        } catch(err) {
+            if (err instanceof MyError) {
+                if (err.message === Errors.INVALID_PAIR) {
+                    throw err;
+                }
+            }
+
+            console.log("Could not update pair", err);
+        }
+    }
 }
 
 const pairsModel = new PairsModel();
 export default pairsModel;
 
 (async () => {
-    await pairsModel.storePair({
-        pair: ["TEST", "TEST2"],
-        price: 0
-    });
+    await pairsModel.updatePairPrice(
+        0.1,
+        ["TEST", "TEST2", "TEST3"],
+    );
     process.exit(0);
 })()
