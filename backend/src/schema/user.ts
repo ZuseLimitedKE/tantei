883ed268fc09fb7ee8c9c9c8ea15f1efe0ca1d5a
Roman Njoroge <roman.njoroge@njuguna.com>
2025-04-25 12:02:47 +0300

    feat: Added regex check for valid hedera wallet address

--- backend/src/schema/user.ts
+++ backend/src/schema/user.ts
@@ -1,12 +1,14 @@
 import z from "zod";
 
+const hederaWalletAddressRegex = /^0\.0\.[A-Za-z0-9]+$/
+
 export const registerUserSchema = z.object({
-  address: z.string(),
+  address: z.string({message: "User address must be a string"}).regex(hederaWalletAddressRegex, {message: "address must be of the form 0.0.xxxx"}),
 });
 
 export const followAgentSchema = z.object({
-  user_hedera_account: z.string(),
-  agent_hedera_account: z.string()
+  user_hedera_account: z.string().regex(hederaWalletAddressRegex, {message: "address must be of the form 0.0.xxxx"}),
+  agent_hedera_account: z.string().regex(hederaWalletAddressRegex, {message: "address must be of the form 0.0.xxxx"})
 })
 
 export type FollowAgent = z.infer<typeof followAgentSchema>;
