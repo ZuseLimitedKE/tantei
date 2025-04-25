2a13f46ef95d87eec99f241d9253ae9ef8000fb6
mbeka02 <antonymbeka@gmail.com>
2025-04-25 20:43:09 +0300

    change _id type

--- ui/src/services/types.ts
+++ ui/src/services/types.ts
@@ -1,23 +1,10 @@
-export interface AgentData {
-  id: string;
-  name: string;
-  avatarUrl: string;
-  description: string;
-  strategyType: string;
-  roi: number;
-  roiTimeframe: string;
-  riskScore: number;
-  followers: number;
-  trades: number;
-  verified: boolean;
-}
 export interface PortfolioStats {
   roi: number;
   value: number;
   last_trade: Date;
 }
 export interface AgentData {
-  _id?: string;
+  _id: string;
   agent_name: string; //the name of the agent
   strategy_description: string; //descripiton of how the agent works,signals , trading approaches etc.
   strategy_type: string; //the categorized trading approach
