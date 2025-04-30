6036a93ab5af68f4204e5a73a0169f6c5fb14228
mbeka02 <antonymbeka@gmail.com>
2025-04-30 14:53:11 +0300

    styling changes for the chatbot ui

--- ui/src/agent/ui/messages/blocks/summary.tsx
+++ ui/src/agent/ui/messages/blocks/summary.tsx
@@ -1,7 +1,7 @@
 export default function Summary(props: { content: string }) {
   const { content } = props;
   return (
-    <div className="w-full p-2 rounded-md bg-[var(--card-background)] border border-[var(--border-color)] text-sm">
+    <div className="w-full p-2 rounded-md bg-[var(--card-background)]   text-sm">
       {content}
     </div>
   );
