5f28ac55ad627dc8f121b9c348becff7a2340220
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2025-04-30 12:32:38 +0300

    feat: Swap functionality

---
+++ backend/src/utils/swaps.ts
@@ -0,0 +1,148 @@
+//Implements Saucerswap swaps
+import {
+    ContractFunctionParameters,
+    ContractExecuteTransaction,
+    Client,
+    Hbar
+
+} from '@hashgraph/sdk';
+import { assert } from 'console';
+import "dotenv/config";
+export interface HBARforToken {
+    amountOutMin: number;
+    tokenPath: string[];
+    toAddress: string;
+    deadline: number;
+    inputHbar: number;
+}
+export interface TokenForHbar {
+    amountIn: number;
+    amountOutMin: number;
+    tokenPath: string[];
+    toAddress: string;
+    deadline: number;
+    inputHbar: number;
+}
+export interface TokenForToken {
+    amountIn: number;
+    amountOutMin: number;
+    tokenPath: string[];
+    toAddress: string;
+    deadline: number;
+}
+export class Swaps {
+    client: Client;
+    OPERATOR_PRIVATE_KEY = process.env.OPERATOR_PRIVATE_KEY;
+    OPERATOR_ID = process.env.OPERATOR_ID;
+    CONTRACT_ID = process.env.SWAP_CONTRACT_ID;
+    GAS_LIMIT = parseInt(process.env.GAS_LIMIT || "1000000", 10);
+    constructor() {
+        if (!process.env.OPERATOR_PRIVATE_KEY || !process.env.OPERATOR_ID || !process.env.SWAP_CONTRACT_ID) {
+            throw new Error("Missing required environment variables: OPERATOR_PRIVATE_KEY or OPERATOR_ID");
+        }
+        this.client = Client.forTestnet().setOperator(this.OPERATOR_ID, this.OPERATOR_PRIVATE_KEY);
+    }
+    /**
+     * Swaps Hbar for token
+     * @param args - The arguments for the swap
+     * @returns - The amount of tokens received
+     * @throws Error if the swap fails
+     */
+    async HBARforToken(args: HBARforToken) {
+        try {
+            assert(args.inputHbar > 0, "Input Hbar must be greater than 0");
+            const params = new ContractFunctionParameters()
+            params.addUint256(args.amountOutMin);
+            params.addAddressArray(args.tokenPath);
+            params.addAddress(args.toAddress);
+            params.addUint256(args.deadline);
+            const response = this.executeSwapEth("swapExactETHForTokens", params, args.inputHbar);
+        }
+        catch (error) {
+            console.log("Error occured while swapping Hbar for token: ", error);
+            throw new Error("Error occured while swapping Hbar for token");
+        }
+    }
+    /**
+     * Swaps Hbar for token
+     * @param args - The arguments for the swap
+     * @returns - The amount of tokens received
+     */
+    async TokensForHBAR(args: TokenForHbar) {
+        try {
+            const params = new ContractFunctionParameters()
+            params.addUint256(args.amountIn);
+            params.addUint256(args.amountOutMin);
+            params.addAddressArray(args.tokenPath);
+            params.addAddress(args.toAddress);
+            params.addUint256(args.deadline);
+            const response = this.executeSwapEth("swapExactTokensForETH", params, args.inputHbar);
+            return response;
+        }
+        catch (error) {
+            console.log("Error occured while swapping token for Hbar: ", error);
+            throw new Error("Error occured while swapping token for Hbar");
+        }
+
+    }
+    /**
+     * Swaps token for token
+     * @param args - The arguments for the swap
+     * @returns The amount of tokens received
+     * @throws Error if the swap fails
+     */
+    async TokensForTokens(args: TokenForToken) {
+        try {
+            const params = new ContractFunctionParameters()
+            params.addUint256(args.amountIn);
+            params.addUint256(args.amountOutMin);
+            params.addAddressArray(args.tokenPath);
+            params.addAddress(args.toAddress);
+            params.addUint256(args.deadline);
+            const response = this.executeSwap("swapExactTokensForTokens", params);
+            return response;
+
+        }
+        catch (error) {
+            console.log("Error occured while swapping token for token: ", error);
+            throw new Error("Error occured while swapping token for token");
+        }
+    }
+    private async executeSwap(functionName: string, params: ContractFunctionParameters) {
+        try {
+            const response = await new ContractExecuteTransaction()
+                .setContractId(this.CONTRACT_ID)
+                .setGas(this.GAS_LIMIT)
+                .setFunction(functionName, params)
+                .execute(this.client);
+
+            const record = await response.getRecord(this.client);
+            const result = record.contractFunctionResult!;
+            const values: [number[]] = result.getResult(['uint[]']);
+            const amounts = values[0];
+            return amounts[amounts.length - 1];
+        } catch (error) {
+            console.error(`Error executing ${functionName} swap:`, error);
+            throw error;
+        }
+    }
+    private async executeSwapEth(functionName: string, params: ContractFunctionParameters, inputHbar: number) {
+        try {
+            const response = await new ContractExecuteTransaction()
+                .setContractId(this.CONTRACT_ID)
+                .setGas(this.GAS_LIMIT)
+                .setPayableAmount(Hbar.fromTinybars(inputHbar))
+                .setFunction(functionName, params)
+                .execute(this.client);
+
+            const record = await response.getRecord(this.client);
+            const result = record.contractFunctionResult!;
+            const values: [number[]] = result.getResult(['uint[]']);
+            const amounts = values[0];
+            return amounts[amounts.length - 1];
+        } catch (error) {
+            console.error(`Error executing swapExactETHForTokens swap:`, error);
+            throw error;
+        }
+    }
+}
---
+++ backend/src/utils/test.ts
@@ -0,0 +1,71 @@
+import { Swaps, HBARforToken, TokenForHbar, TokenForToken } from "./swaps";
+import "dotenv/config";
+import {
+    ContractExecuteTransaction,
+    TokenAssociateTransaction,
+    Hbar,
+    HbarUnit,
+    Client
+} from '@hashgraph/sdk';
+const swap = new Swaps();
+async function getWHBAR() {
+    try {
+        const client = Client.forTestnet().setOperator("0.0.5805516", process.env.OPERATOR_PRIVATE_KEY)
+        const depositHbar = Hbar.from(80, HbarUnit.Hbar)
+
+        await new ContractExecuteTransaction()
+            .setContractId("0.0.5816542")
+            .setPayableAmount(depositHbar)
+            .setGas(1000000)
+            .setFunction('deposit')
+            .execute(client);
+    }
+    catch (error) {
+        console.log("Error occured while getting Wrapped Hbar: ", error);
+        throw new Error("Failed to get Wrapped Hbar");
+    }
+}
+// Swap HBar for token
+export async function HbarToToken() {
+
+    // getWHBAR();
+    console.log("Swapping Hbar for token...");
+    const hbarToTokensargs: HBARforToken = {
+        amountOutMin: 1_000_000,
+        tokenPath: ["0x0000000000000000000000000000000000003aD2", "0x00000000000000000000000000000000000014F5", "0x00000000000000000000000000000000003991eD"],
+        toAddress: "0x1ba65df9858d82e66cbf0574964f1704aa8c7ea9",
+        deadline: Math.floor(Date.now() / 1000) + 60 * 20,
+        inputHbar: 1_000_000_000
+    }
+    const h2tResponse = await swap.HBARforToken(hbarToTokensargs);
+    console.log("Hbar to Token Response: ", h2tResponse);
+}
+
+// Swap token for HBar
+export async function TokenToHbar() {
+    console.log("Swapping token for Hbar...");
+    const tokensToHbarargs: TokenForHbar = {
+        amountIn: 100000,
+        amountOutMin: 1,
+        tokenPath: ["0x000000000000000000000000000000000058441F","0x00000000000000000000000000000000005860C0"],
+        toAddress: "0.0.19266",
+        deadline: Math.floor(Date.now() / 1000) + 60 * 20,
+        inputHbar: 1000000
+    }
+    const t2hResponse = await swap.TokensForHBAR(tokensToHbarargs);
+    console.log("Token to Hbar Response: ", t2hResponse);
+}
+
+// Swap token for token
+export async function TokenToToken() {
+    console.log("Swapping token for token...");
+    const tokensToTokensargs: TokenForToken = {
+        amountIn: 1,
+        amountOutMin: 1,
+        tokenPath: ["0x000000000000000000000000000000000058441F", "0x00000000000000000000000000000000005860C0"],
+        toAddress: "0x1ba65df9858d82e66cbf0574964f1704aa8c7ea9",
+        deadline: Math.floor(Date.now() / 1000) + 60 * 20
+    }
+    const t2tResponse = await swap.TokensForTokens(tokensToTokensargs);
+    console.log("Token to Token Response: ", t2tResponse);
+}
