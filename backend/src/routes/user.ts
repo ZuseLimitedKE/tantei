5e1d9d1d172aab657e4a239cfb9af573646e69eb
Roman Njoroge <roman.njoroge@njuguna.com>
2025-04-30 19:01:13 +0300

    feat: Route for copying trades

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -7,6 +7,10 @@ import agentController from "../controllers/agent";
 import smartContract from "../model/smart_contract";
 import pairsModel from "../model/pairs";
 import tokenModel from "../model/tokens";
+import { error } from "console";
+import swapsController from "../controllers/swaps";
+import userModel from "../model/users";
+import swapsModel from "../model/swap";
 const router: Router = Router();
 
 router.post("/register", async(req, res) => {
@@ -100,6 +104,23 @@ router.get("/tokens/:user_wallet", async (req, res) => {
         }
     } catch(err) {
         console.error("Error getting user's tokens in route", err);
+        res.status(500).json({error: Errors.INTERNAL_SERVER_ERROR});
+    }
+});
+
+router.get("/trades/:user_wallet", async(req, res) => {
+    try {
+        const user_wallet = req.params.user_wallet as string;
+        const parsed = hederaAddress.safeParse(user_wallet);
+        if (parsed.success) {
+            const trades = await swapsController.getUserTrades({hedera_address: parsed.data}, userModel, smartContract, swapsModel);
+            res.json(trades);
+        } else {
+            const errors = parsed.error.issues.map((i) => i.message);
+            res.status(400).json({error: errors});
+        }
+    } catch(err) {
+        console.error("Error getting users trades in request", err);
         res.status(500).json({error: Errors.INTERNAL_SERVER_ERROR});
     }
 })
