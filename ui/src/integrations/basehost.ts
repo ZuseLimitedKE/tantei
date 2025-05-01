9581cde95b636c50cae3df221aaba3082f9a0caa
mbeka02 <antonymbeka@gmail.com>
2025-05-01 22:18:13 +0300

    base host fix

--- ui/src/integrations/basehost.ts
+++ ui/src/integrations/basehost.ts
@@ -1,1 +1,6 @@
-export const BASEHOST = import.meta.env.VITE_ENVIRONMENT === "DEV" ? "http://localhost:7000" : "http://tantei.zuse.co.ke/";
+const devUrl = import.meta.env.VITE_DEV_BACKEND_URL;
+const prodUrl = import.meta.env.VITE_PROD_BACKEND_URL;
+
+export const BASEHOST = import.meta.env.DEV
+  ? devUrl // Dev backend
+  : prodUrl; // Prod backend
